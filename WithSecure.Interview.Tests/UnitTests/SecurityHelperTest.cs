fe3cb484d6d41377a1e7c099103df6a642c1ec6f
shahrzad jahanbaz <shahrzadjahanbaz@gmail.com>
2022-07-17 17:57:55 +0300

    fix bugs!

--- WithSecure.Interview.Tests/UnitTests/SecurityHelperTest.cs
+++ WithSecure.Interview.Tests/UnitTests/SecurityHelperTest.cs
@@ -1,7 +1,7 @@
 ﻿using FluentAssertions;
 using WithSecure.Interview.Common.Helper;
 
-namespace WithSecure.Interview.Api.Tests
+namespace WithSecure.Interview.Api.Tests.UnitTests
 {
     public class SecurityHelperTest
     {
@@ -11,7 +11,7 @@ namespace WithSecure.Interview.Api.Tests
             //Arrange
             var array = new byte[0];
             //Act
-            Action action = () =>  SecurityHelper.CalculateSHA1(array);
+            Action action = () => SecurityHelper.CalculateSHA1(array);
             //Assert
             action.Should().Throw<Exception>().WithMessage("ByteArray for calculate SHA1 can not be null."); ;
         }
@@ -21,7 +21,7 @@ namespace WithSecure.Interview.Api.Tests
             //Arrange
             var array = new byte[5] { 1, 2, 3, 4, 5 };
             //Act
-            var result= SecurityHelper.CalculateSHA1(array);
+            var result = SecurityHelper.CalculateSHA1(array);
             //Assert
             var resultArray = new byte[20] { 17, 150, 106, 185, 192, 153, 248, 250, 190, 250, 197, 76, 8, 213, 190, 43, 216, 201, 3, 175 };
             result.Should().BeEquivalentTo(resultArray);
